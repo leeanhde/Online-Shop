d4b1fa3a51dde33282d80e0e9f84c00896e1d0fe
leeanhde <114909280+leeanhde@users.noreply.github.com>
2023-02-13 23:14:53 +0700

    DBContext

--- Assignment/src/java/Dal/DBContext.java
+++ Assignment/src/java/Dal/DBContext.java
@@ -4,10 +4,24 @@
  */
 package Dal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author anhde
  */
 public class DBContext {
     
+	public Connection getConnection() throws Exception {	       
+            String url = "jdbc:sqlserver:// " + serverName+":" + portNumber+";databaseName="+dbName;
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            return DriverManager.getConnection(url, userID, password);
+        }
+        private final String serverName = "localhost";
+        private final String dbName = "Database_assignment";
+        private final String portNumber = "1433";
+        private final String userID = "anhde";
+        private final String password = "anhde";
 }
+
