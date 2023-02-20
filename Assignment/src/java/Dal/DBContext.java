5f7b490af216d88bd621a3af6378c61afb9d4347
leeanhde <114909280+leeanhde@users.noreply.github.com>
2023-02-21 04:16:02 +0700

    DBContext

--- Assignment/src/java/Dal/DBContext.java
+++ Assignment/src/java/Dal/DBContext.java
@@ -6,22 +6,34 @@ package Dal;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ArrayList;
 
 /**
  *
  * @author anhde
  */
-public class DBContext {
-    
-	public Connection getConnection() throws Exception {	       
-            String url = "jdbc:sqlserver:// " + serverName+":" + portNumber+";databaseName="+dbName;
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            return DriverManager.getConnection(url, userID, password);
-        }
-        private final String serverName = "localhost";
-        private final String dbName = "Database_assignment";
-        private final String portNumber = "1433";
-        private final String userID = "anhde";
-        private final String password = "anhde";
-}
+public abstract class DBContext<T> {
+    protected Connection connection;
+    private final String serverName = "localhost";
+    private final String dbName = "Database_assignment";
+    private final String portNumber = "1433";
+    private final String userID = "anhde";
+    private final String password = "anhde";
 
+    public DBContext() {
+
+    }
+
+    public Connection getConnection() throws Exception {
+        String url = "jdbc:sqlserver:// " + serverName + ":" + portNumber + ";databaseName=" + dbName;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+    
+    public abstract void insert(T model); //INSERT INTO
+    public abstract void update(T model); //UPDATE SET
+    public abstract void delete(T model); //DELETE FROM
+    public abstract T get(int id); //SELECT * FROM WHERE id =
+    public abstract ArrayList<T> list();//SELECT * FROM
+    
+}
